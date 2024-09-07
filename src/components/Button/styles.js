63309b7c5a87cd50657213ba44b62a06da61c7fb
Alan Felipe <alanfelipe1678@gmail.com>
2024-09-06 21:18:45 -0300

    commit inicial

---
+++ src/components/Button/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 2px solid 	#4F4F4F;
+    border-radius: 10px;
+    background-color: #A9A9A9;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+    
+    &:hover {
+        opacity: 0.6;
+    }
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+    margin-bottom: 3px;
+
+    
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #778899;
+        border: 2px solid 	#C0C0C0;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        padding: 0 10px;
+        font-size: 24px;
+        font-family: 'Roboto';
+        color: #FFFFFF;
+
+        
+
+        
+    }
+`
